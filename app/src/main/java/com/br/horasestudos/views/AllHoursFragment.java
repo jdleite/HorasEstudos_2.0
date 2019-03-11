3529a3468657984f7d02d230303cc8faa1b786a6
Jefferson de Deus Leite <jd.leitee@gmail.com>
2019-03-11 20:27:56 -0300

    Feito a parte das fragments e as chamadas dos mesmos no menu lateral

---
+++ app/src/main/java/com/br/horasestudos/views/AllHoursFragment.java
@@ -0,0 +1,28 @@
+package com.br.horasestudos.views;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.br.horasestudos.R;
+
+
+public class AllHoursFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_all_hours, container, false);
+    }
+
+}
